feae09ab65c28e9f237d345afbc3e3e942c6c3b1
ProDCG <mason.stuart05@outlook.com>
2022-03-17 08:58:57 -0700

    QOL changes

--- 3DPhysicsEngine/engine.cpp
+++ 3DPhysicsEngine/engine.cpp
@@ -1,6 +1,7 @@
 #include <glad.h>
 #include <glfw3.h>
 #include <stb_image.h>
+#include <shader_impl.cpp>
 
 #include <GLM/glm/glm.hpp>
 #include <GLM/glm/gtc/matrix_transform.hpp>
@@ -107,15 +108,6 @@ int main()
     // world space positions of our cubes
     glm::vec3 cubePositions[] = {
         glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
