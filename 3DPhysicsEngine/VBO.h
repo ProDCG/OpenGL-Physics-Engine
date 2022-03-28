3c17a4ba2fb4e21e6b6b4b5c4f2435313103e655
ProDCG <mason.stuart05@outlook.com>
2022-03-28 09:11:49 -0700

    More project setup

---
+++ 3DPhysicsEngine/VBO.h
@@ -0,0 +1,19 @@
+#ifndef VBO_CLASS_H
+#define VBO_CLASS_H
+
+#include <glad/glad.h>
+
+class VBO {
+public:
+	GLuint ID;
+
+	VBO(GLfloat* vertices, GLsizeiptr size);
+
+	void Bind();
+
+	void Unbind();
+
+	void Delete();
+};
+
+#endif
