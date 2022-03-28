6785e6ef7b028e2c6e95d0fc232aaa785edc4a95
ProDCG <mason.stuart05@outlook.com>
2022-03-28 08:49:35 -0700

    Deleting most files, they're causing more issues then help, taking a different approach.

---
+++ 3DPhysicsEngine/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
