97e64373ea01950a775c16a6645d32c545353e08
ProDCG <mason.stuart05@outlook.com>
2022-03-29 11:01:12 -0700

    Vector and Matrix setup for camera movement.

--- 3DPhysicsEngine/Main.cpp
+++ 3DPhysicsEngine/Main.cpp
@@ -22,6 +22,13 @@ const unsigned int height = 800;
 
 const unsigned int rotational_multiplier = 17;
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
 GLfloat vertices[] = {
 	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
 	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
