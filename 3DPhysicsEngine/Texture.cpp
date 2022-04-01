1d8d42eb0d7d7e520df04940c8983cfd7650d556
ProDCG <mason.stuart05@outlook.com>
2022-04-01 09:05:24 -0700

    Texture wrap changes, attempting to fix bug with underside wrapping not working as intended

--- 3DPhysicsEngine/Texture.cpp
+++ 3DPhysicsEngine/Texture.cpp
@@ -15,8 +15,10 @@ Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, 
 	glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+	glTexParameteri(texType, GL_TEXTURE_WRAP_R, GL_REPEAT);
 	glTexParameteri(texType, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(texType, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
 
 	glTexImage2D(texType, 0, GL_RGBA, widthImg, heightImg, 0, format, pixelType, bytes);
 	glGenerateMipmap(texType);
