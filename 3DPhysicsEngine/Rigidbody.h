d66be555398f4527d295181bb962a61b0c5d7b43
Mason Stuart <mason.stuart05@outlook.com>
2022-05-19 09:39:39 -0700

    integral

--- 3DPhysicsEngine/Rigidbody.h
+++ 3DPhysicsEngine/Rigidbody.h
@@ -20,7 +20,9 @@ public:
 	float momentOfInertia;
 
 	void calculateInertia() {
-		momentOfInertia = 
+		momentOfInertia = cgnrtr5kju54oi40y4y4iy94u4u546iu4uoi6ou66ou6ouo6uo6ioiouoiooi6oi6uoi6oi6uoi6uo6uiu6oi6iou6oui46u44i4ti4u41ti5551''
+			uywuyorwuygr1u51i526nt52e6g52a;l416416416d16e51r6i51v6i51t6a51v6i51v6e516516516159526256951e41u6le51r6ulerulerulerulr65165165166labs656565
+			nullptr_t
 	}
 };
 
