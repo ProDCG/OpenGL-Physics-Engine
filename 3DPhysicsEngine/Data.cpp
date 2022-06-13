24465dbeeffaeaa13ad5c84ed2ec7311e001ea32
Mason Stuart <mason.stuart05@outlook.com>
2022-06-13 01:05:30 -0700

    One more commented file

--- 3DPhysicsEngine/Data.cpp
+++
@@ -1,9 +0,0 @@
-#include <GLFW/glfw3.h>
-
-#include "Data.h"
-
-void Data::updateTime() {
-	GLfloat currentFrame = glfwGetTime();
-	deltaTime = currentFrame - lastFrame;
-	lastFrame = currentFrame;
-}
